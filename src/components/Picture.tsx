cf1265112c8498810608ff8fc22b9312b6240f38
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-05 18:49:37 +0200

    tweak path for images

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -3,9 +3,12 @@ import { cn } from "@/lib/utils";
 export default function Picture() {
   return (
     <picture className="w-full md:w-1/2">
-      <source media="(min-width:768px)" srcSet="/image-product-desktop.jpg" />
+      <source
+        media="(min-width:768px)"
+        srcSet="/product-preview-card-component/image-product-desktop.jpg"
+      />
       <img
-        src="/image-product-mobile.jpg"
+        src="/product-preview-card-component/image-product-mobile.jpg"
         alt="Photo of perfume"
         className={cn(
           "h-[240px] w-auto rounded-t-xl bg-cover bg-center",
