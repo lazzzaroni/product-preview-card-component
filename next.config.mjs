2d4cf38e8c696699e10214c02b273e2121c87c3e
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-05 18:46:08 +0200

    tweak for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  basePath: "/product-preview-card-component",
   reactStrictMode: true,
 
   /**
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,24 +17,24 @@ export default function Home() {
         <link
           rel="shortcut icon"
           type="image/x-icon"
-          href="/images/favicon.ico" // add baseUrl for GutHub
+          href="/product-preview-card-component/images/favicon.ico"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/images/apple-touch-icon.png" // add baseUrl for GutHub
+          href="/product-preview-card-component/images/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/images/favicon-32x32.png" // add baseUrl for GutHub
+          href="/product-preview-card-component/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/images/favicon-16x16.png" // add baseUrl for GutHub
+          href="/product-preview-card-component/images/favicon-16x16.png"
         />
       </Head>
       <div
